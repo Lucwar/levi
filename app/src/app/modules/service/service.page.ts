a0f68ee50f335cd67d43733357565c30367cc99d
Lucas Guerra <lucas@diproach.com>
2023-12-02 13:56:11 -0300

    Avances

--- app/src/app/modules/service/service.page.ts
+++ app/src/app/modules/service/service.page.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { BasePage } from 'src/app/core/base.page';
+import { ItemPage } from 'src/app/core/item.page';
 
 @Component({
   selector: 'app-service',
   templateUrl: './service.page.html',
   styleUrls: ['./service.page.scss'],
 })
-export class ServicePage extends BasePage{
+export class ServicePage extends ItemPage{
 
-    /* SEGMENT */
-    segmentSong: string = 'details';
-    segmentNotes: string = 'employees';
-    segmentValue: string = this.segmentSong;
-
+  endPoint: string;
+  dateFrom;
 
     goToSong(){
       this.pageService.navigateRoute('song/view/1')
