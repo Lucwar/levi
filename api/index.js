5695110835b138f37b0635330e1ec07d14dc8034
Lucas Guerra <lucas@diproach.com>
2024-08-20 23:09:11 -0300

    test indexjs

--- api/index.js
+++ api/index.js
@@ -41,17 +41,40 @@ const start = async () => {
     // Si hay un error en la conexión a MongoDB, mostrar el error y no iniciar el servidor
     debug('Error connecting to MongoDB:', e);
     process.exit(1);
-  }
-};
+  } finally {
 
-mongoose.connection.close(false, (err) => {
-  if (err) {
-    console.error('Error closing MongoDB connection:', err);
-  } else {
-    console.log('MongoDB connection closed');
+    // process.on('unhandledRejection', payload => log({ message: 'unhandledRejection', level: 'error' }, { name: payload }));
+
+    [`SIGINT`, `SIGUSR1`, `SIGUSR2`, `uncaughtException`, `SIGTERM`].forEach(event => process.on(event, () => gracefulShutdown(event)));
+
+    serverHTTPS.listen(settings.portHTTPS);
+
+    serverHTTP.listen(settings.port, () => {
+
+      settings.checkEnvironmentVariables();
+
+      console.log('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+      console.log(`
+                    Puerto HTTP: ${settings.port}
+                    Puerto HTTPS: ${settings.portHTTPS}
+                    Environment: ${process.env.NODE_ENV}
+                    Fecha: ${new Date().toLocaleString()}
+                  `);
+      console.log('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||');
+    });
   }
-  console.log('---------------------------------------------------------------');
-  process.exit(0);
-});
+}
+
+const gracefulShutdown = async event => {
+  mongoose.connection.close(false, (err) => {
+    if (err) {
+      console.error('Error closing MongoDB connection:', err);
+    } else {
+      console.log('MongoDB connection closed');
+    }
+    console.log('---------------------------------------------------------------');
+    process.exit(0);
+  });
+};
 
 start();
